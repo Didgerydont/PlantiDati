8f34c6e74f3df16f53e32c4847cddd6773e3ace7
Will Prendergast <prendygast@hotmail.com>
2023-06-30 00:24:10 +0100

    Second commit

---
+++ src/main/java/com/project/springboot/plantidati/PlantidatiApplication.java
@@ -0,0 +1,14 @@
+package com.project.springboot.plantidati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantidatiApplication {
+
+	public static void main(String[] args) {
+		// This line bootsraps the application
+		SpringApplication.run(PlantidatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springboot/plantidati/PlantidatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot.plantidati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantidatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
