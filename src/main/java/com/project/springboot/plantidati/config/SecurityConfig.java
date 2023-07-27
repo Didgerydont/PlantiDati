6c105e93de807b25e64e5105cee78cdd7cfca5f7
Will Prendergast <prendygast@hotmail.com>
2023-07-27 02:09:28 +0100

    Still working on Security

--- src/main/java/com/project/springboot/plantidati/config/SecurityConfig.java
+++ src/main/java/com/project/springboot/plantidati/config/SecurityConfig.java
@@ -1,45 +1,43 @@
 package com.project.springboot.plantidati.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests()
-//                // .requestMatchers("/index").authenticated()
-//                .anyRequest().permitAll();
-//        http.formLogin().loginPage("/login")
-//                .and().logout().logoutSuccessUrl("/logout");
-//        return http.build();
-//    }
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
-//    @Bean
-//    InMemoryUserDetailsManagerConfigurer users() {
-//        return new InMemoryUserDetailsManagerConfigurer(
-//        );
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 
-//    @Bean
-//    JdbcUserDetailsManager users(DataSource dataSource) {
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        return jdbcUserDetailsManager;
-//    }
 
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(csrf -> csrf.ignoringRequestMatchers())
-//                .authorizeRequests(auth -> auth.anyRequest()
-//                        .authenticated()
-//                )
-//                .formLogin(withDefaults())
-//                .build();
-//    }
+        return http.build();
+    }
 
 }
